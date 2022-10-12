5ce37548747a4eb8ca32fd4194d70a024496356d
sericaer <sericaer@outlook.com>
2022-10-12 22:43:57 +0800

    change the wetnessMap Builder

--- Assets/Depends/HuangD.Maps/WetnessMapBuilder.cs
+++ Assets/Depends/HuangD.Maps/WetnessMapBuilder.cs
@@ -10,23 +10,29 @@ namespace HuangD.Maps
     {
         internal static Dictionary<(int x, int y), float> Build(Dictionary<(int x, int y), HashSet<(int x, int y)>> riverBanks, Dictionary<(int x, int y), float> rainMap, Dictionary<(int x, int y), TerrainType> terrains)
         {
-            var plusMap = rainMap.Select(p =>
+            var plusMap = new Dictionary<(int x, int y), float>();
+
+            foreach(var pair in rainMap)
             {
-                var Value = p.Value;
-                if(riverBanks.ContainsKey(p.Key))
+                var Value = pair.Value;
+                if (riverBanks.ContainsKey(pair.Key))
                 {
-                    Value += 0.6f;
+                    Value += 0.2f;
+                    plusMap[pair.Key] = Value;
 
-                    foreach(var neighor in Hexagon.GetNeighbors(p.Key).Where(n=> !riverBanks.ContainsKey(n) && rainMap.ContainsKey(n)))
+                    foreach (var neighor in Hexagon.GetNeighbors(pair.Key).Where(n => !riverBanks.ContainsKey(n) && rainMap.ContainsKey(n)))
                     {
-                        Value += 0.3f;
+                        plusMap[pair.Key] = rainMap[neighor] + 0.1f;
                     }
-
                 }
 
-                return (Key:p.Key, Value:Value);
-            });
-
+                if(!plusMap.ContainsKey(pair.Key))
+                {
+                    plusMap.Add(pair.Key, Value);
+                }
+                
+            }
+            
             var maxValue = plusMap.Max(x => x.Value);
 
             return plusMap.ToDictionary(k => k.Key, v => v.Value / maxValue);
